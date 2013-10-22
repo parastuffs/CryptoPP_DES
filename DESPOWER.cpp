61d20fcdefb7845d6f9e7d1b436ebed83c6bc480
parastuffs <parastuffs@gmail.com>
2013-10-23 00:31:03 +0200

    Conversion BYTE <-> BITSET is working. Still need to verify if the decyphered message correspond to the plaintext.

--- DESPOWER.cpp
+++ DESPOWER.cpp
@@ -8,6 +8,7 @@
 
 #include <iomanip>
 #include <cstdio>
+#include <bitset>
 
 using namespace std;
 
@@ -17,8 +18,17 @@ int main(int argc, char* argv[])
 {
 	 
 	//Code from codeproject
-	byte plainText[] = {'H','E','L','L','O',' ','W','O','R','L','D',
-		0x0,0x0,0x0,0x0,0x0};
+	//byte plainText[] = {'H','E','L','L','O',' ','W','O','R','L','D',
+	//	0x0,0x0,0x0,0x0,0x0};
+	
+	//Il se peut que j'aie un probleme avec le fait que byte n'est pas un tableau
+	//de bytes (de 8 bits). Il faudrait alors que je me démerde pour parcourir le
+	//bitset et le couper en paquets de 8 bits. Utiliser sizeof(bitset) ?
+	bitset<12> password(string("101010101010"));
+	bitset<16> extendedPass(string("0000101010101010"));//Test with "0000" concatenated with password.to_string
+	bitset<8> passA(string("00001010"));
+	bitset<8> passB(string("10101010"));
+	byte plainText[] = {passA.to_ulong(),passB.to_ulong()};
 	byte key[CryptoPP::DES::DEFAULT_KEYLENGTH];
 	::memset(key,0x01,CryptoPP::DES::DEFAULT_KEYLENGTH);
 	
@@ -41,6 +51,20 @@ int main(int argc, char* argv[])
 	cout << "Cypher text(" << sizeof(cbCypheredText) << " bytes)" << endl;
 	cout << "Recovered text(" << sizeof(cbRecoveredText) << " bytes): ";
 	cout << "'" << cbRecoveredText << "'" << endl;
+
+	cout << "### Passing a BITESET to a BYTE" << endl;
+
+	bitset<8> word(string("01000001"));//A
+	byte wordByte = word.to_ulong();
+	cout << "word: '" << word << "'" << endl;
+	cout << "wordByte: '" << wordByte << "'" << endl;
+
+	cout << "### Trying to pass a BYTE to a BITSET" << endl;
+	byte secondWord = 'B';
+	bitset<8> secondBitset = secondWord;
+	cout << "secondWord: '" << secondWord << "'" << endl;
+	cout << "secondBitset: '" << secondBitset << "'" << endl;
+	
 
 	//Code from the cryptoPP's wiki
 /*
